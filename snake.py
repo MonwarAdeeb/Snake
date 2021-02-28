f1cfb726a9d72c3de17c76134b69d87f8535c2b8
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-02-28 07:31:15 +0600

    Worked on Snake

--- snake.py
+++ snake.py
@@ -10,3 +10,14 @@ height = 500
 
 cols = 25
 rows = 20
+
+
+class cube():
+    rows = 20
+    w = 500
+
+    def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
+        self.pos = start
+        self.dirnx = dirnx
+        self.dirny = dirny  # "L", "R", "U", "D"
+        self.color = color
