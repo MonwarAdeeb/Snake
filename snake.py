2844aca5aa408ca12da311a1d4e0f3ad4d5a514d
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-03-12 09:25:50 +0600

    Worked on Snake

--- snake.py
+++ snake.py
@@ -145,3 +145,13 @@ def drawGrid(w, rows, surface):
 
 def randomSnack(rows, item):
     positions = item.body
+
+    while True:
+        x = random.randrange(1, rows-1)
+        y = random.randrange(1, rows-1)
+        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
+            continue
+        else:
+            break
+
+    return (x, y)
